094ecbb0ea6f325ac9e41f2f79cc80f969f6cc38
shirley24k <kuik0224@gmail.com>
2024-03-05 23:01:30 +0800

    Create wishlist.php

--- templates/navbar.php
+++ templates/navbar.php
@@ -1,12 +1,12 @@
 <nav>
     <ul>
-        <li><a href="/webasgm/index">Home</a></li>
-        <li><a href="/webasgm/product">Product</a></li>
-        <li><a href="/webasgm/contact">Contact Us</a></li>
-        <li><img src="/webasgm/images/watsis_personal_care.png" alt="Watsis logo" style="width: 200px; height: 110px;"></li>
-        <li><a href="/webasgm/about">About Us</a></li>
-        <li><a href="/webasgm/faq">Account</a></li>
-        <li><img src="/webasgm/images/shopping_bag_icon.png" alt="Shopping Bag" style="width: 50px; height: 50px;"></li>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="product.php">Product</a></li>
+        <li><a href="contact.php">Contact Us</a></li>
+        <li><img src="images/watsis_personal_care.png" alt="Watsis logo" style="width: 200px; height: 110px;"></li>
+        <li><a href="about.php">About Us</a></li>
+        <li><a href="account.php">Account</a></li>
+        <li><img src="images/shopping_bag_icon.png" alt="Shopping Bag" style="width: 50px; height: 50px;"></li>
     </ul>
 </nav>
     
---
+++ wishlist.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Wishlist</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <!--Nav Bar-->
+
+    <!--Wishlist-->
+    <section class="wishlist">
+
+        <h2 class="heading">Wishlist</h2>
+        
+        <div class="box_container">
+            
+            <div class="box">
+                <div class="product">
+                    <span class="stock_status">In Stock</span>
+                    <div class="image">
+                        <img src="images/hairoil1.png" alt="product image">
+                    </div>
+                    <div class="content">
+                        <h3>Shiseido Macherie Hair Oil 60ml</h3>
+                        <p>RM 69.00</p>
+                    </div>
+                </div>
+                <div class="btn">
+                    <a href="#" class="addcartbtn">Add to Cart</a>
+                    <a href="#" class="removebtn">Remove</a>
+                </div>
+            </div>
+            
+            <div class="box">
+                <div class="product">
+                    <span class="stock_status">In Stock</span>
+                    <div class="image">
+                        <img src="images/hairoil1.png" alt="product image">
+                    </div>
+                    <div class="content">
+                        <h3>Shiseido Macherie Hair Oil 60ml</h3>
+                        <p>RM 69.00</p>
+                    </div>
+                </div>
+                <div class="btn">
+                    <a href="#" class="addcartbtn">Add to Cart</a>
+                    <a href="#" class="removebtn">Remove</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="product">
+                    <span class="stock_status">In Stock</span>
+                    <div class="image">
+                        <img src="images/hairoil1.png" alt="product image">
+                    </div>
+                    <div class="content">
+                        <h3>Shiseido Macherie Hair Oil 60ml</h3>
+                        <p>RM 69.00</p>
+                    </div>
+                </div>
+                <div class="btn">
+                    <a href="#" class="addcartbtn">Add to Cart</a>
+                    <a href="#" class="removebtn">Remove</a>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="product">
+                    <span class="stock_status">Out of Stock</span>
+                    <div class="image">
+                        <img src="images/hairoil1.png" alt="product image">
+                    </div>
+                    <div class="content">
+                        <h3>Shiseido Macherie Hair Oil 60ml</h3>
+                        <p>RM 69.00</p>
+                    </div>
+                </div>
+                <div class="btn">
+                    <a href="#" class="addcartbtn">Add to Cart</a>
+                    <a href="#" class="removebtn">Remove</a>
+                </div>
+            </div>
+        </div>
+
+                
+    </section>
+
+    <!--Footer-->
+
+</body>
+</html>
+
