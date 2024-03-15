f163aa82269ac95d49a3e61adee2dd59da0002de
dingxing03 <162273164+dingxing03@users.noreply.github.com>
2024-03-15 18:34:22 +0800

    create itemList.php and update with style.css

---
+++ itemList.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Item</title>
+    <link rel="stylesheet" href="style.css">
+	<script src="https://kit.fontawesome.com/34fcbc38f7.js" crossorigin="anonymous"></script>
+</head>
+<body>
+    <!--Nav Bar-->
+
+    <!--Item List-->
+	<section id="product1" class="section-p1">
+		<h2>Product List</h2>
+		<p>Personal Care</p>
+		<div class="pro-container">
+			<div class ="pro">
+				<img src="images/bodywash1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>Jo Malone Lime Basil & Mandarin Body & Hand Wash</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM385.00</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+			<div class ="pro">
+				<img src="images/soap1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>Chanel N°5 The Bath Soap</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM135.00</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+			<div class ="pro">
+				<img src="images/bodyoil1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>The Ritual Of Sakura Body oil</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM125.00</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+			<div class ="pro">
+				<img src="images/bodycream1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>CLOUD MILK Coconut + Maca Firming Body Cream</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM191.95</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+			<div class ="pro">
+				<img src="images/soap1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>Jo Malone Lime Basil & Mandarin Body & Hand Wash</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM385.00</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+			<div class ="pro">
+				<img src="images/soap1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>Jo Malone Lime Basil & Mandarin Body & Hand Wash</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM125.00</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+			<div class ="pro">
+				<img src="images/soap1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>Jo Malone Lime Basil & Mandarin Body & Hand Wash</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM.00</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+			<div class ="pro">
+				<img src="images/soap1.png" alt="">
+				<div class= "des">
+					<span>Watsis</span>
+					<h5>Jo Malone Lime Basil & Mandarin Body & Hand Wash</h5>
+					<div class="star">
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+						<i class="fas fa-star"></i>
+					</div>
+					<h4>RM191.95.00</h4>
+				</div>
+				<a href="#"><i class="fa-solid fa-cart-shopping cart"></i></a>
+			</div>
+		</div>
+	</section>
+	
+	<!-- Promotion -->
+	<section id="banner" class="section-m1">
+		<h4>Promotion</h4>
+		<h2>Up to <span>90% Off<span>-All categories</h2>
+		<button>Explore more</button>
+	</section>
+
+</body>
+</html>
