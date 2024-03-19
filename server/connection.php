152ecc3f92edaed36e2ab99298df4b490cdefe69
shirley24k <kuik0224@gmail.com>
2024-03-19 22:45:25 +0800

    create connection.php and dbtables.sql
    connection.php is used to connect to the server. dbtables.sql is used to write the sql command for create table and insert value into the table, the command will be copy and paste to the phpmyadmin

---
+++ server/connection.php
@@ -0,0 +1,22 @@
+<?php
+//Database configuration
+$dbHost='localhost';
+$dbUsername='root';
+$dbPassword='';
+$dbName='dbwatsis';
+
+//Create database connection
+$conn = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbName);
+
+//Check if the connection is successful
+if(!$conn){
+    die('Could not connect to the database: '.mysqli_connect_error());
+}
+else{
+    echo 'Connection successful!';
+}
+
+//Close the connection
+mysqli_close($conn);
+
+?>
