bafa1b9253474c38bcb4f29b4ba31d914a4208fa
BernardHwang <147620186+BernardHwang@users.noreply.github.com>
2024-03-20 10:49:39 +0800

    upload homepage

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Watsis Personal Care</title>
+        <link rel="stylesheet" href="style.css">
+        <script src="script.js"></script>
+    </head>
+    <body>
+        <?php include('../Asgm/header.php') ?>
+        <div class="homepage-scroll-container">
+            <a href=""><img src="png/anceserum1.png" alt="acneserum"></a>
+            <a href=""><img src="png/bodywash1.png" alt="bodywash"></a>
+            <a href=""><img src="png/cleanser1.png" alt="cleanser"></a>
+            <a href=""><img src="png/conditioner1.png" alt="conditioner"></a>
+            <a href=""><img src="png/hairmask1.png" alt="hairmask"></a>
+            <a href=""><img src="png/mouthwash1.png" alt="mouthwash"></a>
+            <a href=""><img src="png/shampoo1.png" alt="shampoo"></a>
+            <a href=""><img src="png/soap1.png" alt="soap"></a>
+            <a href=""><img src="png/toner1.png" alt="toner"></a>
+            <a href=""><img src="png/toothpaste1.png" alt="toothpaste"></a>
+        </div>
+        <button class="scroll-button scroll-button-left" onclick="scrollSmooth('left')">&#10094;</button>
+        <button class="scroll-button scroll-button-right" onclick="scrollSmooth('right')">&#10095;</button>
+        <?php include('../Asgm/footer.php') ?>
+    </body>
+</html>
