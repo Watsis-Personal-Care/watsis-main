9341b8ee811ca23f369868ace1882eb790429911
shirley24k <kuik0224@gmail.com>
2024-03-24 21:34:19 +0800

    change file and image path

--- index.php
+++ index.php
@@ -6,42 +6,42 @@
         <script src="script.js"></script>
     </head>
     <body>
-        <?php include('../Asgm/header.php') ?>
+        <?php include('templates\header.php') ?>
         <div class="hompage-product">
             <p class="popular-product">Popular Product</p>
             <div class="homepage-scroll-container">
                 <div class="image-container">
-                    <a href=""><img src="png/anceserum1.png" alt="acneserum"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\anceserum1.png" alt="acneserum"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/bodywash1.png" alt="bodywash"><div class="overlay-homepage-img">Body Wash</div></a>
+                    <a href=""><img src="images\bodywash1.png" alt="bodywash"><div class="overlay-homepage-img">Body Wash</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/cleanser1.png" alt="cleanser"><div class="overlay-homepage-img">Cleanser</div></a>
+                    <a href=""><img src="images\cleanser1.png" alt="cleanser"><div class="overlay-homepage-img">Cleanser</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/conditioner1.png" alt="conditioner"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\conditioner1.png" alt="conditioner"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/hairmask1.png" alt="hairmask"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\hairmask1.png" alt="hairmask"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/mouthwash1.png" alt="mouthwash"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\mouthwash1.png" alt="mouthwash"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/shampoo1.png" alt="shampoo"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\shampoo1.png" alt="shampoo"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/soap1.png" alt="soap"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\soap1.png" alt="soap"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/toner1.png" alt="toner"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\toner1.png" alt="toner"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div>
                 <div class="image-container">
-                    <a href=""><img src="png/toothpaste1.png" alt="toothpaste"><div class="overlay-homepage-img">Acne Serum</div></a>
+                    <a href=""><img src="images\toothpaste1.png" alt="toothpaste"><div class="overlay-homepage-img">Acne Serum</div></a>
                 </div> 
             </div>
         </div>
-        <?php include('../Asgm/footer.php') ?>
+        <?php include('templates\footer.php') ?>
     </body>
 </html>
