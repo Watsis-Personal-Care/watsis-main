028845017c04d88a04b4b4dd1e621d1df3d6446c
dingxing03 <162273164+dingxing03@users.noreply.github.com>
2024-03-28 16:42:07 +0800

    update index

--- index.php
+++ index.php
@@ -6,45 +6,27 @@
         <script src="script.js"></script>
     </head>
     <body>
-        <?php include('templates\header.php') ?>
+    <?php include('templates/header.php') ?>
         <div class="hompage-product">
-            <p class="popular-product">Popular Product</p>
+            <p class="popular-product">Popular Products</p>
             <div class="homepage-scroll-container">
-                <div class="image-container">
-                    <a href=""><img src="images\anceserum1.png" alt="acneserum"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\bodywash1.png" alt="bodywash"><div class="overlay-homepage-img">Body Wash</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\cleanser1.png" alt="cleanser"><div class="overlay-homepage-img">Cleanser</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\conditioner1.png" alt="conditioner"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\hairmask1.png" alt="hairmask"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\mouthwash1.png" alt="mouthwash"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\shampoo1.png" alt="shampoo"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\soap1.png" alt="soap"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\toner1.png" alt="toner"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div>
-                <div class="image-container">
-                    <a href=""><img src="images\toothpaste1.png" alt="toothpaste"><div class="overlay-homepage-img">Acne Serum</div></a>
-                </div> 
-            </div>
-            <div class="homepage-discovermore">
-                <a href="itemList.php">Discover More</a>
+            <?php include('server/get_featured_products.php');
+            while($row = $featured_products->fetch_assoc()) { ?>
+                <a href="itemDetails.php?product_id=<?php echo $row['product_id']; ?>">
+                    <div class="image-container">
+                        <img src="<?php echo $row['product_image1']; ?>" alt="">
+                        <div class="overlay-homepage-img">
+                            <h5><?php echo $row['product_name']; ?></h5>
+                        </div>
+                    </div>
+                </a>
+                <?php } ?>
             </div>
         </div>
+    <div class="homepage-discovermore">
+        <a href="itemList.php">Discover More</a>
+    </div>
+
         <?php include('templates\footer.php') ?>
     </body>
 </html>
