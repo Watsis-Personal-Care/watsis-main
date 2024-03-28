566ddf1c61460aa57ca86d35dad6466ecd1c62ef
BernardHwang <147620186+BernardHwang@users.noreply.github.com>
2024-03-28 21:11:08 +0800

    Update index.php add desc for promote prod and add back the scroller hover stuff

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <title>Watsis Personal Care</title>
+        <script src="https://kit.fontawesome.com/34fcbc38f7.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="style.css">
         <script src="script.js"></script>
     </head>
@@ -9,9 +10,39 @@
         <?php include('templates/header.php') ?>
         <div class="homepage">
             <img src="images/homepage_pic.png" alt="homepage_pic" width="100%">
-            <div class="promote-prod">
-                <a href=""><img src="images/promote_prod1" alt="promote_prod1"></a>
-                <a href=""><img src="images/promote_prod2" alt="promote_prod2"></a>
+            <div class="promote-prods" style="margin-top: 50px">
+                <div class="promote-prod">
+                    <a href=""><img src="images/promote_prod1" alt="promote_prod1"></a>
+                    <div class="promote-prod-desc">
+                        <p style="text-align: justify">
+                            "Is your hair feeling flat and lifeless? Does it lack volume? 
+                            You're not alone. 
+                            But there is a solution. Introducing Grafen Root Booster Shampoo, 
+                            the special edition formula that will transform your hair."
+                        </p>
+                        
+                        <p style="text-align:right">
+                            <i class="fa-solid fa-minus"></i>Grafen Root Booster Shampoo
+                        </p>
+                    </div>
+                    
+                </div>
+                <div class="promote-prod">
+                    <div class="promote-prod-desc">
+                        <p style="text-align: justify">
+                            "Indulge in the transformative power of The Ritual of Sakura body oil. 
+                            Enriched with cherry blossom extract and rice milk, 
+                            this luxurious oil hydrates, nourishes and leaves your 
+                            skin feeling irresistibly soft and smooth."
+                        </p>
+                        <p style="text-align:right">
+                            <i class="fa-solid fa-minus"></i>The Ritual Of Sakura Body oil
+                        </p>
+                        </div>
+                    
+                    <a href=""><img src="images/promote_prod2" alt="promote_prod2"></a>
+                    
+                </div>
             </div>
             <div class="brief-aboutus">
                 <div class="briefing">
@@ -28,11 +59,18 @@
                 </div>
             </div>
             <div class="hompage-product">
-                <p class="popular-product">Featured Product</p>
+                <p class="Featured-product" style="margin-left: 20px">Featured Product</p>
                 <div class="homepage-scroll-container">
                     <?php include('server/get_featured_products.php');
                     while($row = $featured_products->fetch_assoc()) { ?>
-                        <a href="itemDetails.php?product_id=<?php echo $row['product_id']; ?>"><img src="<?php echo $row['product_image1']; ?>" alt=""></a>
+                    <a href="itemDetails.php?product_id=<?php echo $row['product_id']; ?>">
+                        <div class="image-container">
+                            <img src="<?php echo $row['product_image1']; ?>" alt="">
+                            <div class="overlay-homepage-img">
+                                <h5><?php echo $row['product_name']; ?></h5>
+                            </div>
+                        </div>
+                    </a>
                     <?php } ?>
                 </div>
             </div>
