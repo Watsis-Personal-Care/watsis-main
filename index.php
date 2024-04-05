e61c9984302f85000f27fde72f1c52b43008eb1b
shirley24k <kuik0224@gmail.com>
2024-04-05 16:02:35 +0800

    modify homepage

--- index.php
+++ index.php
@@ -19,33 +19,33 @@
                         <a href="itemList.php">Explore</a>
                 </nav>
             </div>
-            <div class="promote-prods" style="margin-top: 50px">
+            <div class="promote-prods">
                 <div class="promote-prod">
                     <a href="itemDetails.php?product_id=6"><img src="images/promote_prod1" alt="promote_prod1"></a>
                     <div class="promote-prod-desc">
-                        <p  style="text-align: justify; font-size: 30px;">
+                        <p class="description">
                             "Is your hair feeling flat and lifeless? Does it lack volume? 
                             You're not alone. 
                             But there is a solution. Introducing Grafen Root Booster Shampoo, 
                             the special edition formula that will transform your hair."
                         </p>
                         
-                        <p style="text-align:right">
-                            <i class="fa-solid fa-minus"></i>Grafen Root Booster Shampoo
+                        <p class="prod-name">
+                            —— Grafen Root Booster Shampoo
                         </p>
                     </div>
                     
                 </div>
                 <div class="promote-prod">
                     <div class="promote-prod-desc">
-                        <p style="text-align: justify; font-size: 30px;">
+                        <p class="description">
                             "Indulge in the transformative power of The Ritual of Sakura body oil. 
                             Enriched with cherry blossom extract and rice milk, 
                             this luxurious oil hydrates, nourishes and leaves your 
                             skin feeling irresistibly soft and smooth."
                         </p>
-                        <p style="text-align:right">
-                            <i class="fa-solid fa-minus"></i>The Ritual Of Sakura Body oil
+                        <p class="prod-name">
+                            —— The Ritual of Sakura Body Oil
                         </p>
                         </div>
                     
@@ -54,33 +54,39 @@
                 </div>
             </div>
             <div class="brief-aboutus">
-                <div class="briefing">
-                    <img src="images/mission_icon.png" alt="mission_icon">
-                    <p>Making it an essential part of everyday life.</p>
+                <div class="brief-mission">
+                    <p>— Our mission —</p>
                 </div>
-                <div class="briefing">
-                    <img src="images/product_icon.png" alt="product_icon">
-                    <p>Provides top-quality products</p>
-                </div>
-                <div class="briefing">
-                    <img src="images/customer_satisfaction_icon.png" alt="customer_satisfaction_icon">
-                    <p>Your satisfaction is our priority</p>
+                <div class="briefings">
+                    <div class="briefing">
+                        <img src="images/mission_icon.png" alt="mission_icon">
+                        <p>Making it an essential part of everyday life.</p>
+                    </div>
+                    <div class="briefing">
+                        <img src="images/product_icon.png" alt="product_icon">
+                        <p>Provides top-quality products</p>
+                    </div>
+                    <div class="briefing">
+                        <img src="images/customer_satisfaction_icon.png" alt="customer_satisfaction_icon">
+                        <p>Your satisfaction is our priority</p>
+                    </div>
                 </div>
             </div>
-            <div class="hompage-product">
-                <p class="Featured-product" style="margin-left: 20px">Featured Product</p>
+            <div class="homepage-product">
+                <p class="Featured-product">— Featured Product —</p>
                 <div class="homepage-scroll-container">
-                    <?php include('server/get_featured_products.php');
-                    while($row = $featured_products->fetch_assoc()) { ?>
-                    <a href="itemDetails.php?product_id=<?php echo $row['product_id']; ?>">
-                        <div class="image-container">
-                            <img src="<?php echo $row['product_image1']; ?>" alt="">
-                            <div class="overlay-homepage-img">
-                                <h5><?php echo $row['product_name']; ?></h5>
-                            </div>
+                <?php include('server/get_featured_products.php');
+                while($row = $featured_products->fetch_assoc()) { ?>
+                <a href="itemDetails.php?product_id=<?php echo $row['product_id']; ?>">
+                    <div class="image-container">
+                        <img src="<?php echo $row['product_image1']; ?>" alt="">
+                        <div class="overlay-homepage-img">
+                            <h5><?php echo $row['product_name']; ?></h5>
                         </div>
-                    </a>
-                    <?php } ?>
+                    </div>
+                </a>
+                <?php } ?>
+
                 </div>
             </div>
 
--- templates/footer.php
+++ templates/footer.php
@@ -12,15 +12,26 @@
       <div class="footer-left">
         <p>More Links</p>
         <nav class="footernav-left">
-            <a href="#">About Us</a>
-            <a href="itemList.php">Product</a>
-            <a href="#">Contact</a>
+            <a href="aboutus.php">About Us</a>
+            <a href="itemList.php">Products</a>
+            <a href="Contact.php">Contact Us</a>
+            <a href="wishlist.php">Wishlist</a>
+            <a href="cart.php">Cart</a>
         </nav>
         <img src="images/Watsis_xbg.png" alt="logo" width = "180px">
       </div>
 
       <div class="footer-center">
+        <p>Products</p>
+        <nav class="footernav-center">
+          <a href="itemList.php#BodyCare">Body Care</a>
+          <a href="itemList.php#DentalCare">Dental Care</a>
+          <a href="itemList.php#HairCare">Hair Care</a>
+          <a href="itemList.php#SkinCare">Skin Care</a>
+        </nav>
+        <p class="copyright">
         &copy;2024 Watsis Personal Care Berhad. All rights reserved.
+        </p>
       </div>
 
       <div class="footer-right">
