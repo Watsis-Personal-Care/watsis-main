9381e2ad57a3d82c434558e135a030be62084f64
shirley24k <kuik0224@gmail.com>
2024-04-05 21:47:41 +0800

    logout session

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+// Start a session
+session_start();
+
+// Check if the user is logged in
+if(!isset($_SESSION['user_id'])) {
+  // Redirect the user to the login page
+  header("Location: signinupform.php");
+  exit;
+}
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
