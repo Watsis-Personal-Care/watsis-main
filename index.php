3e4d4d392df177e0310c56450ccb0b05ddec3f92
shirley24k <kuik0224@gmail.com>
2024-04-15 08:47:35 +0800

    home page small side nav

--- index.php
+++ index.php
@@ -13,6 +13,8 @@ if(!isset($_SESSION['user_id'])) {
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Watsis Personal Care</title>
         <script src="https://kit.fontawesome.com/34fcbc38f7.js" crossorigin="anonymous"></script>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
