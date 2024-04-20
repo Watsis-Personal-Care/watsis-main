7da56c009efc9838fbe22d5bd72567fadf47a308
dingxing03 <162273164+dingxing03@users.noreply.github.com>
2024-04-20 17:25:43 +0800

    update contact.php

--- Contact.php
+++ Contact.php
@@ -1,50 +1,4 @@
 <?php
-
-session_start();
-
-include('server\connection.php');
-
-if (isset($_POST['submitForm'])) {
-    //get info and store it in database
-	$user_id = $_SESSION['user_id'];
-    $name = $_POST['name'];
-    $email = $_POST['email'];
-    $phone = $_POST['phone'];
-    $country = $_POST['country'];  
-	$subject = $_POST['subject'];
-
-    $stmt = $conn->prepare("INSERT INTO contact(user_id, user_name, user_email, user_phone, user_country, subject)
-                    VALUES (?,?,?,?,?,?); ");
-
-    $stmt->bind_param('isssss', $user_id, $name, $email, $phone, $country, $subject);
-
-    $stmt_status= $stmt->execute();
-    if (!$stmt_status){
-        header('location: index.php');
-        exit;
-    }
-}
-?>
-
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>Contact Us Page</title>
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="script.js"></script>
-	<script src="https://kit.fontawesome.com/34fcbc38f7.js" crossorigin="anonymous"></script>
-	<style>
-        .error {
-            color: red;
-        }
-   	</style>
-</head>
-<body>
-	<?php
 	// contact/index.php
 	if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 		// Initialize variables
@@ -85,6 +39,31 @@ if (isset($_POST['submitForm'])) {
 			// Redirect or display a success message
 			echo 'Thank you for contacting us! We will get back to you shortly.';
 			echo "</div>";
+
+			session_start();
+
+		include('server\connection.php');
+
+		if (isset($_POST['submitForm'])) {
+			//get info and store it in database
+			$user_id = $_SESSION['user_id'];
+			$name = $_POST['name'];
+			$email = $_POST['email'];
+			$phone = $_POST['phone'];
+			$country = $_POST['country'];  
+			$subject = $_POST['subject'];
+
+			$stmt = $conn->prepare("INSERT INTO contact(user_id, user_name, user_email, user_phone, user_country, subject)
+							VALUES (?,?,?,?,?,?); ");
+
+			$stmt->bind_param('isssss', $user_id, $name, $email, $phone, $country, $subject);
+
+			$stmt_status= $stmt->execute();
+			if (!$stmt_status){
+				header('location: index.php');
+				exit;
+			}
+		}
 		} else {
 			// Redisplay the form, with error messages
 			include('contact_form.php');
@@ -97,6 +76,26 @@ if (isset($_POST['submitForm'])) {
 		include('contact_form.php');
 	}
 	?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Contact Us Page</title>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha512-SfTiTlX6kk+qitfevl/7LibUOeJWlt9rbyDn92a1DqWOw9vWG2MFoays0sgObmWazO5BQPiFucnnEAjpAB+/Sw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script src="script.js"></script>
+	<script src="https://kit.fontawesome.com/34fcbc38f7.js" crossorigin="anonymous"></script>
+	<style>
+        .error {
+            color: red;
+        }
+   	</style>
+</head>
+<body>
+
 	<script>
 	<?php if (!empty($errors)) { ?>
 		window.onload = function() {
